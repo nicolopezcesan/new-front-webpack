9a1fbec97eb4981a487b4c2ad0c66593871a94ef
nicolopezcesan <nicolopezcesan@gmail.com>
2020-05-21 14:55:02 -0300

    limpieza branch

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,23 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import './index.scss';
-// import URL_ from './services'
-import Articles from './articles';
 
 
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            articles: []
-        };
+        this.state = {};
     }
-
-    componentDidMount() {
-        const fetchPromise = fetch('https://api-test-ln.herokuapp.com/articles');
-        fetchPromise.then(response => {
-            return response.json();
-        }).then(arts => {
-            this.setState({ articles: arts });
-        });
-    }
-
 
     render() {
-        const { articles } = this.state;
-        console.log(articles);
+        const { foo } = this.state;
 
         return (
             <div>
                 <h1>App React</h1>
-                <Articles articles={articles}></Articles>
             </div>
         )
     }
-
 }
 
 // App.propTypes = {
---
+++ src/services/service.js
@@ -0,0 +1,19 @@
+
+
+
+
+// const fetchPromise = fetch('https://api-test-ln.herokuapp.com/articles');
+
+// const getArticles = async () => {
+//     fetchPromise.then(response => {
+//         return response.json();
+//     }).then(people => {
+//         aa = people;
+//         console.log(people);
+//     });
+// }
+
+
+
+
+// export default getArticles;
